31a00af3d3f347dd8880660fb8fe8d39df0b581e
Yueming Sun <cynthia0828@gmail.com>
2016-04-29 12:22:46 -0400

    Update parUsr.r

--- par-usr/parUsr.r
+++ par-usr/parUsr.r
@@ -61,6 +61,34 @@ lgd <- legend(x = mean(c(xmin,xmax)), y =  mean(c(ymin,ymax)),
 legend(x = xmax - lgd$rect$w, y =  ymin + lgd$rect$h,
        c("Your data name"), pch = c(20), col = c("red"), plot = T)
 
+#####################################################################################################################
 
+# also for making pairs plot looks nicer
 
+# will give out correlation matrix in the bottom left triangle
+# have the correlation number in the upper right triangle
+# also define to show statistic significant level
 
+attach(datStata14)
+
+dat <- data.frame(avg_A2A3,Score,A1PercentCorrect,
+                  A2PercentCorrect,A3PercentCorrect,A4PercentCorrect)
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits=digits)[1]
+  txt <- paste(prefix, txt, sep="")
+  if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+  test <- cor.test(x,y)
+  # borrowed from printCoefmat
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+                   cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                   symbols = c("***", "**", "*", ".", " "))
+  text(0.5, 0.5, txt, cex = 1.5 )
+  text(.7, .8, Signif, cex=cex, col=2)
+}
+pairs( dat, lower.panel=panel.smooth, upper.panel=panel.cor)
+
+detach()
