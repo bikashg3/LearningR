99a5d4c74d69d66d91332cbe82216f87b249a249
Yueming Sun <cynthia0828@gmail.com>
2016-04-29 16:11:32 -0400

    Create par-oma.r

---
+++ par-oma.r
@@ -0,0 +1,7 @@
+# oma
+# A vector of the form c(bottom, left, top, right) giving the size of the outer margins in lines of text.
+
+opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
+plot(ols, las = 1)
+
+# www.ats.ucla.edu/stat/r/dae/rreg.htm
