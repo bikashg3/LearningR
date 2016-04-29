018d4acaec775048e2cf40afb8b1c6e1ab799c15
Yueming Sun <cynthia0828@gmail.com>
2016-04-29 16:40:24 -0400

    Delete OpenStata14UsingR-readstata13.r

--- RopenStataV14/OpenStata14UsingR-readstata13.r
+++
@@ -1,8 +0,0 @@
-# https://cran.r-project.org/web/packages/readstata13/README.html
-
-# how to use R to open a Stata dta file with version of older than 12, including version 13 and 14
-
-install.packages("readstata13")
-library(readstata13)
-datStata14 <- read.dta13("Z:/your working directory/A-Net/ANetData/ELAMath2yrMerge_A2A3.dta")
-
