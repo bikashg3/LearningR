ea9a37eea51a110a8e7e8c870c5251a1f1a0c1a4
Yueming Sun <cynthia0828@gmail.com>
2016-05-02 14:34:02 -0400

    Update rGIS-TIGER-ParcelSyr.r

--- rGIS-TIGER-ParcelSyr.r
+++ rGIS-TIGER-ParcelSyr.r
@@ -79,5 +79,184 @@ downtown <- syr[ these.downtown , ]
 
 plot( downtown )
 
+pdf( "Land Use test.pdf" )
 
+
+# Vacant Land
+vacant <- rep( "gray80", nrow(dat) )
+
+vacant[ dat$LandUse == "Vacant Land" ] <- "red" 
+
+plot( syr,  border=NA, col=vacant, main="Vacant" )
+
+
+
+# Single Family
+single.family <- rep( "gray80", nrow(dat) )
+
+# gray.red <- rep( "gray80", nrow(dat) )
+
+single.family[ dat$LandUse == "Single Family" ] <- "red" 
+
+# gray.red[ dat$LandUse == "Single Family" ] <- "red" 
+
+plot( syr,  border=NA, col=single.family, main="Single Family Homes" )
+
+
+# Apartment
+apartment <- rep( "gray80", nrow(dat) )
+
+apartment[ dat$LandUse == "Apartment" ] <- "red" 
+
+plot( syr,  border=NA, col=apartment, main="Apartments" )
+
+
+# Parking
+parking <- rep( "gray80", nrow(dat) )
+
+parking[ dat$LandUse == "Parking" ] <- "red" 
+
+plot( syr,  border=NA, col=parking, main="Parking Lots" )
+
+
+# Commercial and Industrial
+com.ind <- rep( "gray80", nrow(dat) )
+
+com.ind[ dat$LandUse == "Industrial" | dat$LandUse == "Commercial"  ] <- "red" 
+
+plot( syr,  border=NA, col=com.ind, main="Commercial and Industrial")
+
+
+# Schools, Parks & Cemetaries
+park.cem <- rep( "gray80", nrow(dat) )
+
+park.cem[ dat$LandUse == "Schools" | dat$LandUse == "Parks" | dat$LandUse == "Cemetery"  ] <- "red" 
+
+plot( syr,  border=NA, col=park.cem, main="Schools, Parks & Cemetaries" )
+
+
+# Rental Units & Apartments
+landlords <- rep( "gray80", nrow(dat) )
+
+landlords[ dat$LandUse == "Two Family" | dat$LandUse == "Three Family" | dat$LandUse == "Multiple Residence"  | dat$LandUse == "Apartment" ] <- "red" 
+
+plot( syr,  border=NA, col=landlords, main="Rental Units & Apartments" )
+
+
+# Community Services
+comm.services <- rep( "gray80", nrow(dat) )
+
+comm.services[ dat$LandUse == "Community Services"  ] <- "red" 
+
+plot( syr,  border=NA, col=comm.services, main="Community Services" )
+
+
+# Utilities
+utilities <- rep( "gray80", nrow(dat) )
+
+utilities[ dat$LandUse == "Utilities"  ] <- "red" 
+
+plot( syr,  border=NA, col=utilities, main="Utilities" )
+
+
+# Religious
+religious <- rep( "gray80", nrow(dat) )
+
+religious[ dat$LandUse == "Religious"  ] <- "red" 
+
+plot( syr,  border=NA, col=religious, main="Religious" )
+
+
+
+# Property Valued > $100k
+high.price <- rep( "gray80", nrow(dat) )
+
+high.price[ dat$AssessedVa > 100000  ] <- "red" 
+
+plot( syr,  border=NA, col=high.price, main="Property Valued > $100k" )
+
+
+
+
+
+### PRICE PER ACRE
+
+price.acre <- dat$AssessedVa / dat$Acres
+
+price.acre[ price.acre > 10000000 ] <- NA
+
+hist( price.acre, col="gray", xlim=c(0,2000000), breaks=1000 )
+
+
+
+high.val <- rep( "gray80", nrow(dat) )
+
+high.val[ price.acre > 600000  ] <- "red" 
+
+plot( syr,  border=NA, col=high.val, main="Property Valued > $600k/acre" )
+
+
+
+
+# control color by label 
+
+price.groups <- cut( price.acre, breaks=c(0,200000,400000,600000,800000,1000000,10000000), 
+                     labels=c("gray90","gray70","gray50","gray30","gray10","steel blue") )
+
+price.groups <- as.character( price.groups )
+
+plot( syr,  border=price.groups, col=price.groups, bg="white", main="Appraised Property Value" )
+
+tapply( dat$AssessedVa, price.groups, mean, na.rm=T )
+
+
+legend( "bottomright", bg="white",
+        pch=19, pt.cex=1.5, cex=0.7,
+        legend=c("$1,353k","$164k","$114k","$85k","$82k","$54k"), 
+        col=c("steel blue","gray10","gray30","gray50","gray70","gray90"), 
+        box.col="white",
+        title="Average Property Value" 
+       )
+
+
+
+# > tapply( dat$AssessedVa, price.groups, mean, na.rm=T )
+#    gray10    gray30    gray50    gray70    gray90 
+# 163507.99 114350.59  85115.57  82035.95  54325.86
+
+
+
+
+
+
+
+### NON-TAXABLE PARCELS
+
+non.taxable <- rep( "gray80", nrow(dat) )
+
+non.taxable[ dat$LandUse == "Religious" |  dat$LandUse == "Utilities" | dat$LandUse == "Vacant Land" | 
+             dat$LandUse == "Parks" |  dat$LandUse == "Recreation" | dat$LandUse == "Community Services" | 
+             dat$LandUse == "Schools" |  dat$LandUse == "Cemetery"  ] <- "red" 
+
+plot( syr,  border=NA, col=non.taxable, main="Non-Taxable Properties" )
+
+
+tapply( dat$Acres, non.taxable, sum, na.rm=T )
+
+
+tapply( dat$AssessedVa, non.taxable, sum, na.rm=T )
+
+
+dev.off()
+
+
+
+### NEIGHBORHOODS
+
+
+sedgwick <- rep( NA, nrow(dat) )
+
+sedgwick[ dat$Nhood == "Sedgwick"  ] <- "red" 
+
+plot( syr,  border="gray80", col=sedgwick, main="Sedgwick" )
 
